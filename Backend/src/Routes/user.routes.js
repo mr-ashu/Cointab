2224a9de6af6b247161c3926ded83da1797d24c6
Ashutosh Sharma <104469510+mr-ashu@users.noreply.github.com>
2023-02-26 23:24:00 +0530

    backend

--- Backend/src/Routes/user.routes.js
+++ Backend/src/Routes/user.routes.js
@@ -1,42 +1,46 @@
 const express = require("express");
 const User = require("../Schema/user.schema")
 
+const { default: axios } = require("axios");
 const app = express.Router();
  
 
 app.get("/", async (req, res) => {
-    try {
-        let u = await User.find()
-        res.send(u)
-    } catch (er) {
-        res.status(404).send(er.message)
+    let user = await User.find({});
+    if (user.length > 0) {
+        res.send("User already fetch");
+        
+    }
+    else {
+ 
+        var Data;
+        await axios.get("https://randomuser.me/api/?results=100")
+            .then((res) => {
+                Data = res.data.results;
+            })
+            .catch((err) => {
+                console.log("error in fetching the data");
+            })
+        await User.insertMany(Data)
+
+        res.send(`${Data.length} - users successfully added in database`);
     }
 }) 
- 
 
-app.post("/", async (req, res) => {
+app.get("/users", async (req, res) => {
+    const {page,limit,gender } = req.query;
  
-    try {
-     
-     let pdt=await User.create(
-        { ...req.body}
-     )
-     res.send(pdt)
-    
-    } catch (e) {
-       res.status(404).send(e.message)
-    }
- })
+    const users = gender ? await User.find({ gender: gender }).skip((page - 1) * limit).limit(limit) : await User.find({}).skip((page - 1) * limit).limit(limit)
  
- app.delete("/:id", async (req, res) => {
-     let {id}=req.params;
-     try {
-         let u = await User.findByIdAndDelete({_id:id})
-         res.send(u)
-     } catch (e) {
-         res.status(404).send(e.message)
-     }
- });
+    res.send(users);
+})
+
+app.delete("/", async (req, res) => {
+    await User.deleteMany({});
+    res.send(" All Users Deleted Successfully from database");
+})
+ 
+ 
   
 
 
--- Backend/src/Schema/user.schema.js
+++ Backend/src/Schema/user.schema.js
@@ -1,31 +1,16 @@
 const { Schema, model } = require("mongoose");
 const userSchema = new Schema({
-  name: {
-    title:String,
-    first:String,
-    last:String
-  },
-  phone: Number,
-  cell: String,
-  email: String,
-  gender: String,
-  login: {
-    uuid:String,
-    username:String,
-    password:String,
-    salt:String,
-    md5:String
-  },
-  picture: {
-    large:String,
-    medium:String,
-    thumbnail:String,
-     
-  },
-  registered: {
-    age:Number,
-    date:String
-  }
+  gender: { type: String },
+  name: { title: String, first: String, last: String },
+  location: { street: { number: String, name: String }, city: String, state: String, country: String, postcode: String, coordinates: { latitude: String, longitude: String }, timezone: { offset: String, description: String } },
+  email: { type: String },
+  login: { uuid: String, username: String, password: String, salt: String, md5: String, sha1: String, sha256: String },
+  dob: { date: String, age: String },
+  registered: { date: String, age: String },
+  phone: { type: String },
+  cell: { type: String },
+  picture: { large: String, medium: String, thumbnail: String },
+  nat: { type: String }
 
 
 
